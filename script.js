b2334a52ed4f6222a48323df213aa0ee744dd6f7
Rishabh Rawat <rawatrishi1121@gmail.com>
2025-04-28 23:06:30 +0530

    Create script.js
    js

---
+++ script.js
@@ -0,0 +1,5 @@
+ const btn = document.querySelector("button")
+        const h1 = document.querySelector("h1")
+        btn.addEventListener("click", () => {
+            h1.classList.toggle("active")
+        })
